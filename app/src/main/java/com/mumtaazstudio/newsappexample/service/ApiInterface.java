eebf6ceef4d168066b0a95fad8b0f4c4f2e54a37
Muhammad Aswan Abidin <aswanabidin@ymail.com>
2019-11-17 06:36:05 +0700

    add api service

--- app/src/main/java/com/mumtaazstudio/newsappexample/service/ApiInterface.java
+++ app/src/main/java/com/mumtaazstudio/newsappexample/service/ApiInterface.java
@@ -1,4 +1,15 @@
 package com.mumtaazstudio.newsappexample.service;
 
+import com.mumtaazstudio.newsappexample.model.Response;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiInterface {
+    @GET("top-headlines")
+    Call<Response> getLatestNews(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey);
+
 }
