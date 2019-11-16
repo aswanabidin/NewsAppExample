3398797858947609ff7cf8ecc6edc1539ba4438a
Muhammad Aswan Abidin <aswanabidin@ymail.com>
2019-11-17 06:36:26 +0700

    add data model

--- app/src/main/java/com/mumtaazstudio/newsappexample/model/Article.java
+++ app/src/main/java/com/mumtaazstudio/newsappexample/model/Article.java
@@ -1,4 +1,97 @@
 package com.mumtaazstudio.newsappexample.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
 public class Article {
+
+    @SerializedName("source")
+    private Source source;
+
+    @SerializedName("author")
+    private String author;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("urlToImage")
+    private String image;
+
+    @SerializedName("publishedAt")
+    private String published;
+
+    @SerializedName("content")
+    private String content;
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- app/src/main/java/com/mumtaazstudio/newsappexample/model/Response.java
+++ app/src/main/java/com/mumtaazstudio/newsappexample/model/Response.java
@@ -1,4 +1,41 @@
 package com.mumtaazstudio.newsappexample.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Response {
+
+    @SerializedName("status")
+    private String status;
+
+    @SerializedName("totalResults")
+    private String totalResults;
+
+    @SerializedName("articles")
+    private List<Article> articles = null;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
